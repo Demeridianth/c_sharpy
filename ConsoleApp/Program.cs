f7a06f1b547c79bf6374b453a54f8db46378b79d
Demeridianth <100306083+Demeridianth@users.noreply.github.com>
2025-04-19 14:35:29 +0300

    Initial commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello World");
+
+//datatypes
+
+using System.Data;
+using System.Text;
+
+string text = "word";
+int number = 9;
+float starBuckPrice = 2.9f;
+long bigNumber = 2000000000000000000;
+bool isTrue = true;
+
+var someTypeOfData = "data";
+
+
+//string
+var item = "cup";
+Console.WriteLine($"drink from the {item}");
+
+string newItem = item.Replace("cup", "big cup");
+newItem =  newItem.Remove(0, 2);
+Console.WriteLine($"drink from the {newItem}");
+
+StringBuilder newCoolItem = new StringBuilder();
+newCoolItem.Append("very big cup");
+
+//delete        with String BUilder remembers, with simple string, needs a new var
+newCoolItem.Remove(0, 9);
+Console.WriteLine(newCoolItem);
+
+
+//Array
